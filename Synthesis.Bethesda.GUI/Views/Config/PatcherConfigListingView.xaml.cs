e4c3da377be15ffdaee7f4f0d248ed67bca5ad33
Justin Swanson <justin.c.swanson@gmail.com>
2020-11-07 11:34:49 -0600

    Improved error displays

--- Synthesis.Bethesda.GUI/Views/Config/PatcherConfigListingView.xaml.cs
+++ Synthesis.Bethesda.GUI/Views/Config/PatcherConfigListingView.xaml.cs
@@ -7,6 +7,7 @@ using System.Reactive.Linq;
 using System.Text;
 using System.Windows;
 using System.Reactive.Disposables;
+using System.Windows.Controls;
 
 namespace Synthesis.Bethesda.GUI.Views
 {
@@ -32,13 +33,50 @@ namespace Synthesis.Bethesda.GUI.Views
                     .BindToStrict(this, x => x.NameBlock.Text)
                     .DisposeWith(disposable);
 
-                // Set up blocking issue display
+                // Set up tooltip display
                 this.WhenAnyFallback(x => x.ViewModel!.State.RunnableState.Succeeded)
                     .Select(x => x ? Visibility.Collapsed : Visibility.Visible)
                     .BindToStrict(this, x => x.BlockingIssueDisplayCircle.Visibility)
                     .DisposeWith(disposable);
+                //this.WhenAnyFallback(x => x.ViewModel!.DisplayName)
+                //    .BindToStrict(this, x => x.TooltipPatcherName.Text)
+                //    .DisposeWith(disposable);
                 this.WhenAnyFallback(x => x.ViewModel!.State.RunnableState.Reason)
-                    .BindToStrict(this, x => x.BlockingIssueDisplayCircle.ToolTip)
+                    .BindToStrict(this, x => x.TooltipErrorText.Text)
+                    .DisposeWith(disposable);
+                Observable.CombineLatest(
+                    this.WhenAnyValue(x => x.IsMouseOver),
+                    this.WhenAnyFallback(x => x.ViewModel!.State.RunnableState.Succeeded),
+                    (over, succeeded) => over && !succeeded)
+                    .Subscribe(show =>
+                    {
+                        this.SetValue(ToolTipService.IsEnabledProperty, show);
+                    })
+                    .DisposeWith(disposable);
+                this.WhenAnyFallback(x => x.ViewModel!.State.IsHaltingError)
+                    .Select(x => x ? Visibility.Visible : Visibility.Collapsed)
+                    .BindToStrict(this, x => x.ErrorIcon.Visibility)
+                    .DisposeWith(disposable);
+                this.WhenAnyFallback(x => x.ViewModel!.State)
+                    .Select(x => !x.IsHaltingError && x.RunnableState.Failed ? Visibility.Visible : Visibility.Collapsed)
+                    .BindToStrict(this, x => x.ProcessingRing.Visibility)
+                    .DisposeWith(disposable);
+                this.WhenAnyFallback(x => x.ViewModel!.State)
+                    .Select(x => x.RunnableState.Succeeded ? Visibility.Visible : Visibility.Collapsed)
+                    .BindToStrict(this, x => x.SuccessIcon.Visibility)
+                    .DisposeWith(disposable);
+                this.WhenAnyFallback(x => x.ViewModel!.State.RunnableState.Succeeded)
+                    .Select(x => x ? Visibility.Collapsed : Visibility.Visible)
+                    .BindToStrict(this, x => x.TooltipErrorText.Visibility)
+                    .DisposeWith(disposable);
+                this.WhenAnyFallback(x => x.ViewModel!.State)
+                    .Select(x =>
+                    {
+                        if (x.IsHaltingError) return "Blocking Error";
+                        if (x.RunnableState.Failed) return "Processing";
+                        return "Ready";
+                    })
+                    .BindToStrict(this, x => x.StatusTypeText.Text)
                     .DisposeWith(disposable);
 
                 // ContextMenu
