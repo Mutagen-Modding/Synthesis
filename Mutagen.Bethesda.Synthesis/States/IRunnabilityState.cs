adf47f56d196b08e91182b82982df4b7fa9ad78b
Justin Swanson <justin.c.swanson@gmail.com>
2021-01-01 13:49:42 -0600

    Turned off xml comment output for GUI app

--- Mutagen.Bethesda.Synthesis/States/IRunnabilityState.cs
+++ Mutagen.Bethesda.Synthesis/States/IRunnabilityState.cs
@@ -6,7 +6,6 @@ using System.Text;
 namespace Mutagen.Bethesda.Synthesis
 {
     public interface IRunnabilityState
-
     {
         CheckRunnability Settings { get; }
         IEnumerable<LoadOrderListing> LoadOrder { get; }
