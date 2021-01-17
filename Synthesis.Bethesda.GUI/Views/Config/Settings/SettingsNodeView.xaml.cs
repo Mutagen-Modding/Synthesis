4e08aa2628325eaa4c71ee59864210d798e9d369
Justin Swanson <justin.c.swanson@gmail.com>
2021-01-16 18:15:19 -0600

    Highlight borders of settings when focused

--- Synthesis.Bethesda.GUI/Views/Config/Settings/SettingsNodeView.xaml.cs
+++ Synthesis.Bethesda.GUI/Views/Config/Settings/SettingsNodeView.xaml.cs
@@ -1,23 +1,15 @@
-using Noggog.WPF;
-using ReactiveUI;
-using System.Reactive.Disposables;
 using System.Windows.Controls;
 
 namespace Synthesis.Bethesda.GUI.Views
 {
-    public class SettingsNodeViewBase : NoggogUserControl<SettingsNodeVM> { }
-
     /// <summary>
     /// Interaction logic for SettingsNodeView.xaml
     /// </summary>
-    public partial class SettingsNodeView : SettingsNodeViewBase
+    public partial class SettingsNodeView : UserControl
     {
         public SettingsNodeView()
         {
             InitializeComponent();
-            this.WhenActivated(disposable =>
-            {
-            });
         }
     }
 }
