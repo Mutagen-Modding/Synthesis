7b2235aa8a5c463e97fd3f6ead9f4d6e91019b34
Justin Swanson <justin.c.swanson@gmail.com>
2021-01-20 00:48:01 -0600

    nuget bump, and API migrate up

--- Synthesis.Bethesda.GUI/Utility.cs
+++ Synthesis.Bethesda.GUI/Utility.cs
@@ -8,12 +8,8 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.Loader;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Media;
 
 namespace Synthesis.Bethesda.GUI
 {
@@ -45,35 +41,6 @@ namespace Synthesis.Bethesda.GUI
             catch (Exception ex)
             {
                 Log.Logger.Error(ex, $"Error navigating to path: {path}");
-            }
-        }
-
-        class HostAssemblyLoadContext : AssemblyLoadContext
-        {
-            // Resolver of the locations of the assemblies that are dependencies of the
-            // main plugin assembly.
-            private AssemblyDependencyResolver _resolver;
-
-            public HostAssemblyLoadContext(string pluginPath) : base(isCollectible: true)
-            {
-                _resolver = new AssemblyDependencyResolver(pluginPath);
-            }
-
-            // The Load method override causes all the dependencies present in the plugin's binary directory to get loaded
-            // into the HostAssemblyLoadContext together with the plugin assembly itself.
-            // NOTE: The Interface assembly must not be present in the plugin's binary directory, otherwise we would
-            // end up with the assembly being loaded twice. Once in the default context and once in the HostAssemblyLoadContext.
-            // The types present on the host and plugin side would then not match even though they would have the same names.
-            protected override Assembly? Load(AssemblyName name)
-            {
-                string? assemblyPath = _resolver.ResolveAssemblyToPath(name);
-                if (assemblyPath != null)
-                {
-                    Console.WriteLine($"Loading assembly {assemblyPath} into the HostAssemblyLoadContext");
-                    return LoadFromAssemblyPath(assemblyPath);
-                }
-
-                return null;
             }
         }
 
@@ -128,62 +95,13 @@ namespace Synthesis.Bethesda.GUI
             var exec = await DotNetCommands.GetExecutablePath(projPath, cancel);
             if (exec.Failed) return exec.BubbleFailure<TRet>();
             WeakReference hostAlcWeakRef;
-            var ret = ExecuteAndUnload(exec.Value, out hostAlcWeakRef, getter);
+            var ret = AssemblyLoading.ExecuteAndUnload(exec.Value, out hostAlcWeakRef, getter);
             for (int i = 0; hostAlcWeakRef.IsAlive && (i < 10); i++)
             {
                 GC.Collect();
                 GC.WaitForPendingFinalizers();
             }
             return ret;
-        }
-
-        // It is important to mark this method as NoInlining, otherwise the JIT could decide
-        // to inline it into the Main method. That could then prevent successful unloading
-        // of the plugin because some of the MethodInfo / Type / Plugin.Interface / HostAssemblyLoadContext
-        // instances may get lifetime extended beyond the point when the plugin is expected to be
-        // unloaded.
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        static GetResponse<TRet> ExecuteAndUnload<TRet>(string assemblyPath, out WeakReference alcWeakRef, Func<Assembly, GetResponse<TRet>> getter)
-        {
-            // Create the unloadable HostAssemblyLoadContext
-            var alc = new HostAssemblyLoadContext(assemblyPath);
-
-            // Create a weak reference to the AssemblyLoadContext that will allow us to detect
-            // when the unload completes.
-            alcWeakRef = new WeakReference(alc);
-
-            try
-            {
-                // Load the plugin assembly into the HostAssemblyLoadContext.
-                // NOTE: the assemblyPath must be an absolute path.
-                Assembly assemb = alc.LoadFromAssemblyPath(assemblyPath);
-                return getter(assemb);
-            }
-            catch (OperationCanceledException)
-            {
-                return GetResponse<TRet>.Fail("Cancelled");
-            }
-            finally
-            {
-                // This initiates the unload of the HostAssemblyLoadContext. The actual unloading doesn't happen
-                // right away, GC has to kick in later to collect all the stuff.
-                alc.Unload();
-            }
-        }
-
-        public static T? GetChildOfType<T>(this DependencyObject depObj)
-            where T : DependencyObject
-        {
-            if (depObj == null) return null;
-
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
-            {
-                var child = VisualTreeHelper.GetChild(depObj, i);
-
-                var result = (child as T) ?? GetChildOfType<T>(child);
-                if (result != null) return result;
-            }
-            return null;
         }
     }
 }
