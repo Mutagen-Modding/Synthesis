3e4d61fb3519287c6271d599871d8933228b4390
Justin Swanson <justin.c.swanson@gmail.com>
2021-01-25 18:12:40 -0600

    ModKeyBox library updates

--- Synthesis.Bethesda.GUI/ViewModels/Config/Settings/ObjectSettingsVM.cs
+++ Synthesis.Bethesda.GUI/ViewModels/Config/Settings/ObjectSettingsVM.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reflection;
 using System.Text.Json;
 
 namespace Synthesis.Bethesda.GUI
