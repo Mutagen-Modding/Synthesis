6e8afbb437afe92a5d99eb4fd27643b8822f2e61
Justin Swanson <justin.c.swanson@gmail.com>
2021-02-23 23:07:30 -0600

    SynthesisDescription - no implementation

---
+++ Mutagen.Bethesda.Synthesis/Settings Attributes/SynthesisDescription.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mutagen.Bethesda.Synthesis.Settings
+{
+    [AttributeUsage(
+      AttributeTargets.Field | AttributeTargets.Property,
+      AllowMultiple = false)]
+    public class SynthesisDescription : Attribute
+    {
+        public string Text { get; }
+
+        public SynthesisDescription(string text)
+        {
+            Text = text;
+        }
+    }
+}
