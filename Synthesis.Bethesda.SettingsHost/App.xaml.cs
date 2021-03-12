29172f509f5478d306298fe9941c29499eda7d24
Justin Swanson <justin.c.swanson@gmail.com>
2021-03-11 20:33:23 -0600

    SettingsHost

---
+++ Synthesis.Bethesda.SettingsHost/App.xaml.cs
@@ -0,0 +1,52 @@
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Synthesis.Bethesda.SettingsHost
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var result = Parser.Default.ParseArguments(e.Args, typeof(HostSettings))
+                .MapResult<HostSettings, int>(
+                    (host) =>
+                    {
+                        DoHostSettings(host);
+                        return 0;
+                    },
+                    _ => { return -1; });
+
+            if (result == -1)
+            {
+                Application.Current.Shutdown(result);
+            }
+        }
+
+        private void DoHostSettings(HostSettings host)
+        {
+            var window = new MainWindow();
+            var vm = new MainVM(host);
+            window.DataContext = vm;
+            window.Closed += (a, b) =>
+            {
+                vm.Save();
+            };
+            window.Left = host.Left;
+            window.Top = host.Top;
+            window.Width = host.Width;
+            window.Height = host.Height;
+            window.Show();
+        }
+    }
+}
