a0433bde92a6364a394969dbab03eb4be42053a9
Justin Swanson <justin.c.swanson@gmail.com>
2021-04-17 21:58:25 -0500

    IRunnabilityState.GetEnvironmentState

--- Mutagen.Bethesda.Synthesis/States/IRunnabilityState.cs
+++ Mutagen.Bethesda.Synthesis/States/IRunnabilityState.cs
@@ -1,13 +1,41 @@
 using Synthesis.Bethesda;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Mutagen.Bethesda.Synthesis
 {
+    /// <summary>
+    /// An interface housing all the tools, parameters, and entry points for checking if a patcher is runnable
+    /// </summary>
     public interface IRunnabilityState
     {
+        [EditorBrowsable(EditorBrowsableState.Never)]
         CheckRunnability Settings { get; }
+
+        /// <summary>
+        /// A list of ModKeys as they appeared, and whether they were enabled
+        /// </summary>
         IEnumerable<LoadOrderListing> LoadOrder { get; }
+
+        /// <summary>
+        /// Path to the plugins.txt used
+        /// </summary>
+        string LoadOrderFilePath { get; }
+
+        /// <summary>
+        /// Path to the game data folder
+        /// </summary>
+        string DataFolderPath { get; }
+
+        /// <summary>
+        /// GameRelease targeted for patching
+        /// </summary>
+        GameRelease GameRelease { get; }
+
+        GameEnvironmentState<TModSetter, TModGetter> GetEnvironmentState<TModSetter, TModGetter>()
+            where TModSetter : class, IContextMod<TModSetter, TModGetter>, TModGetter
+            where TModGetter : class, IContextGetterMod<TModSetter, TModGetter>;
     }
 }
--- Mutagen.Bethesda.Synthesis/States/RunnabilityState.cs
+++ Mutagen.Bethesda.Synthesis/States/RunnabilityState.cs
@@ -23,12 +23,29 @@ namespace Mutagen.Bethesda.Synthesis
         public IReadOnlyList<LoadOrderListing> LoadOrder { get; }
         IEnumerable<LoadOrderListing> IRunnabilityState.LoadOrder => this.LoadOrder;
 
+        public string LoadOrderFilePath => Settings.LoadOrderFilePath;
+
+        public string DataFolderPath => Settings.DataFolderPath;
+
+        public GameRelease GameRelease => Settings.GameRelease;
+
         public RunnabilityState(
             CheckRunnability settings,
             IReadOnlyList<LoadOrderListing> rawLoadOrder)
         {
             Settings = settings;
             LoadOrder = rawLoadOrder;
+        }
+
+        public GameEnvironmentState<TModSetter, TModGetter> GetEnvironmentState<TModSetter, TModGetter>()
+            where TModSetter : class, IContextMod<TModSetter, TModGetter>, TModGetter
+            where TModGetter : class, IContextGetterMod<TModSetter, TModGetter>
+        {
+            var lo = Mutagen.Bethesda.LoadOrder.Import<TModGetter>(DataFolderPath, LoadOrder, GameRelease);
+            return new GameEnvironmentState<TModSetter, TModGetter>(
+                gameFolderPath: DataFolderPath,
+                loadOrder: lo,
+                linkCache: lo.ToImmutableLinkCache<TModSetter, TModGetter>());
         }
     }
 }
