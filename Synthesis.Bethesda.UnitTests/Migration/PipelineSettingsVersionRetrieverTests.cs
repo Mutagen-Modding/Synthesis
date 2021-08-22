c78af0d195395ae8be620ea5504ed7e533c4b541
Justin Swanson <justin.c.swanson@gmail.com>
2021-08-22 12:04:34 -0500

    Some Pipeline settings upgrade tests

---
+++ Synthesis.Bethesda.UnitTests/Migration/PipelineSettingsVersionRetrieverTests.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using FluentAssertions;
+using Noggog;
+using Synthesis.Bethesda.Execution.Settings.Json.Pipeline;
+using Xunit;
+
+namespace Synthesis.Bethesda.UnitTests.Migration
+{
+    public class PipelineSettingsVersionRetrieverTests
+    {
+        [Fact]
+        public void Version1Detection()
+        {
+            new PipelineSettingsVersionRetriever(IFileSystemExt.DefaultFilesystem)
+                .GetVersion("Migration/PipelineSettings.json")
+                .Should().Be(1);
+        }
+        
+        [Fact]
+        public void Version2Detection()
+        {
+            new PipelineSettingsVersionRetriever(IFileSystemExt.DefaultFilesystem)
+                .GetVersion("Migration/PipelineSettings.v2.json")
+                .Should().Be(2);
+        }
+    }
+}
---
+++ Synthesis.Bethesda.UnitTests/Migration/V1toV2Tests.cs
@@ -0,0 +1,37 @@
+﻿using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Noggog;
+using Synthesis.Bethesda.Execution.Settings.Json.Pipeline;
+using Synthesis.Bethesda.Execution.Settings.Json.Pipeline.V1;
+using Synthesis.Bethesda.Execution.Settings.Json.Pipeline.V2;
+using Xunit;
+
+namespace Synthesis.Bethesda.UnitTests.Migration
+{
+    public class V1toV2Tests
+    {
+        public PipelineSettingsImporter MakeImporter(IFileSystem fs)
+        {
+            return new PipelineSettingsImporter(
+                new PipelineSettingsV1Reader(fs),
+                new PipelineSettingsV2Reader(fs),
+                new PipelineSettingsUpgrader(
+                    new PipelineSettingsV1Upgrader()),
+                new PipelineSettingsVersionRetriever(fs));
+        }
+
+        [Fact]
+        public void Upgrade()
+        {
+            var import = MakeImporter(IFileSystemExt.DefaultFilesystem)
+                .Import("Migration/PipelineSettings.json");
+            var mockFileSystem = new MockFileSystem();
+            mockFileSystem.File.WriteAllText("C:/Test", JsonConvert.SerializeObject(import, Formatting.Indented, Synthesis.Bethesda.Execution.Constants.JsonSettings));
+            var expected = IFileSystemExt.DefaultFilesystem.File.ReadAllText("Migration/PipelineSettings.v2.json");
+            var reimport = mockFileSystem.File.ReadAllText("C:/Test");
+            reimport.Should().Be(expected);
+        }
+    }
+}
