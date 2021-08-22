a4c2249612a0a687917d65ad66fbe1d772024688
Justin Swanson <justin.c.swanson@gmail.com>
2021-08-22 12:27:24 -0500

    Old settings backup systems

--- Synthesis.Bethesda.Execution/Settings/Json/Pipeline/PipelineSettingsImporter.cs
+++ Synthesis.Bethesda.Execution/Settings/Json/Pipeline/PipelineSettingsImporter.cs
@@ -15,24 +15,29 @@ namespace Synthesis.Bethesda.Execution.Settings.Json.Pipeline
         private readonly IPipelineSettingsV1Reader _v1Reader;
         private readonly IPipelineSettingsV2Reader _v2Reader;
         private readonly IPipelineSettingsUpgrader _upgrader;
+        private readonly IPipelineSettingsBackup _backup;
         private readonly IPipelineSettingsVersionRetriever _versionRetriever;
 
         public PipelineSettingsImporter(
             IPipelineSettingsV1Reader v1Reader,
             IPipelineSettingsV2Reader v2Reader,
             IPipelineSettingsUpgrader upgrader,
+            IPipelineSettingsBackup backup,
             IPipelineSettingsVersionRetriever versionRetriever)
         {
             _v1Reader = v1Reader;
             _v2Reader = v2Reader;
             _upgrader = upgrader;
+            _backup = backup;
             _versionRetriever = versionRetriever;
         }
 
         public IPipelineSettings Import(FilePath path)
         {
+            var vers = _versionRetriever.GetVersion(path);
+            
             object o;
-            switch (_versionRetriever.GetVersion(path))
+            switch (vers)
             {
                 case 1:
                     o = _v1Reader.Read(path);
@@ -42,6 +47,11 @@ namespace Synthesis.Bethesda.Execution.Settings.Json.Pipeline
                     break;
                 default:
                     throw new NotImplementedException();
+            }
+
+            if (vers != 2)
+            {
+                _backup.Backup(vers, path);
             }
 
             return _upgrader.Upgrade(o);
--- Synthesis.Bethesda.UnitTests/Migration/V1toV2Tests.cs
+++ Synthesis.Bethesda.UnitTests/Migration/V1toV2Tests.cs
@@ -1,4 +1,6 @@
-﻿using System.IO.Abstractions;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
 using FluentAssertions;
 using Newtonsoft.Json;
@@ -12,26 +14,44 @@ namespace Synthesis.Bethesda.UnitTests.Migration
 {
     public class V1toV2Tests
     {
-        public PipelineSettingsImporter MakeImporter(IFileSystem fs)
+        private const string V1FilePath = "C:/PipelineSettings.json";
+        private const string V1FilePathBackup = "C:/PipelineSettings.v1.json";
+        
+        public PipelineSettingsImporter MakeImporter(out MockFileSystem fileSystem)
         {
+            fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>()
+            {
+                { V1FilePath, File.ReadAllText("Migration/PipelineSettings.json") }
+            });
             return new PipelineSettingsImporter(
-                new PipelineSettingsV1Reader(fs),
-                new PipelineSettingsV2Reader(fs),
+                new PipelineSettingsV1Reader(fileSystem),
+                new PipelineSettingsV2Reader(fileSystem),
                 new PipelineSettingsUpgrader(
                     new PipelineSettingsV1Upgrader()),
-                new PipelineSettingsVersionRetriever(fs));
+                new PipelineSettingsBackup(fileSystem),
+                new PipelineSettingsVersionRetriever(fileSystem));
         }
 
         [Fact]
         public void Upgrade()
         {
-            var import = MakeImporter(IFileSystemExt.DefaultFilesystem)
-                .Import("Migration/PipelineSettings.json");
-            var mockFileSystem = new MockFileSystem();
+            var import = MakeImporter(out var mockFileSystem)
+                .Import(V1FilePath);
             mockFileSystem.File.WriteAllText("C:/Test", JsonConvert.SerializeObject(import, Formatting.Indented, Synthesis.Bethesda.Execution.Constants.JsonSettings));
             var expected = IFileSystemExt.DefaultFilesystem.File.ReadAllText("Migration/PipelineSettings.v2.json");
             var reimport = mockFileSystem.File.ReadAllText("C:/Test");
             reimport.Should().Be(expected);
+        }
+        
+        [Fact]
+        public void MakesV1Backup()
+        {
+            MakeImporter(out var mockFileSystem)
+                .Import(V1FilePath);
+            mockFileSystem.File.Exists(V1FilePathBackup)
+                .Should().BeTrue();
+            mockFileSystem.File.ReadAllText(V1FilePath).Should().Be(
+                mockFileSystem.File.ReadAllText(V1FilePathBackup));
         }
     }
 }
