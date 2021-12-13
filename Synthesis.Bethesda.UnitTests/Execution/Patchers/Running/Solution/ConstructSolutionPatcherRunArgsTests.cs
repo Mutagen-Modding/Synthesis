3ba64204e35d042e0592791c9860d84cf1fc0c5c
Justin Swanson <justin.c.swanson@gmail.com>
2021-12-12 22:06:02 -0600

    Fix for unit tests

--- Synthesis.Bethesda.UnitTests/Execution/Patchers/Running/Solution/ConstructSolutionPatcherRunArgsTests.cs
+++ Synthesis.Bethesda.UnitTests/Execution/Patchers/Running/Solution/ConstructSolutionPatcherRunArgsTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using Mutagen.Bethesda.Strings;
 using Noggog;
 using NSubstitute;
 using Synthesis.Bethesda.Commands;
@@ -12,9 +13,11 @@ namespace Synthesis.Bethesda.UnitTests.Execution.Patchers.Running.Solution
     {
         [Theory, SynthAutoData]
         public void SettingsForwarding(
+            Language language,
             RunSynthesisPatcher settings,
             ConstructSolutionPatcherRunArgs sut)
         {
+            settings.TargetLanguage = language.ToString();
             var ret = sut.Construct(settings);
             ret.DataFolderPath.Should().Be(settings.DataFolderPath);
             ret.GameRelease.Should().Be(settings.GameRelease);
@@ -28,9 +31,11 @@ namespace Synthesis.Bethesda.UnitTests.Execution.Patchers.Running.Solution
         [Theory, SynthAutoData]
         public void SetsExtraDataToProviderResult(
             DirectoryPath dir,
+            Language language,
             RunSynthesisPatcher settings,
             ConstructSolutionPatcherRunArgs sut)
         {
+            settings.TargetLanguage = language.ToString();
             sut.PatcherExtraDataPathProvider.Path.Returns(dir);
             sut.Construct(settings)
                 .ExtraDataFolder.Should().Be(dir);
@@ -39,9 +44,11 @@ namespace Synthesis.Bethesda.UnitTests.Execution.Patchers.Running.Solution
         [Theory, SynthAutoData]
         public void DefaultDataFolderPathNullIfDoesNotExist(
             DirectoryPath missingDirectory,
+            Language language,
             RunSynthesisPatcher settings,
             ConstructSolutionPatcherRunArgs sut)
         {
+            settings.TargetLanguage = language.ToString();
             sut.DefaultDataPathProvider.Path.Returns(missingDirectory);
             sut.Construct(settings)
                 .DefaultDataFolderPath.Should().BeNull();
@@ -50,9 +57,11 @@ namespace Synthesis.Bethesda.UnitTests.Execution.Patchers.Running.Solution
         [Theory, SynthAutoData]
         public void DefaultDataFolderPathSetByProviderIfExists(
             DirectoryPath existingDirectory,
+            Language language,
             RunSynthesisPatcher settings,
             ConstructSolutionPatcherRunArgs sut)
         {
+            settings.TargetLanguage = language.ToString();
             sut.DefaultDataPathProvider.Path.Returns(existingDirectory);
             sut.Construct(settings)
                 .DefaultDataFolderPath.Should().Be(existingDirectory);
