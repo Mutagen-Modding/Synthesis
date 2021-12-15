00b7e46146ed7fb5e75bfc82a30411fffd35fa0c
Justin Swanson <justin.c.swanson@gmail.com>
2021-12-14 22:37:50 -0600

    Fix for new profile dialog

--- Synthesis.Bethesda.GUI/Views/Profiles/NewProfileView.xaml.cs
+++ Synthesis.Bethesda.GUI/Views/Profiles/NewProfileView.xaml.cs
@@ -20,10 +20,15 @@ namespace Synthesis.Bethesda.GUI.Views
                 this.Bind(this.ViewModel, vm => vm.Nickname, view => view.PatcherDetailName.Text)
                     .DisposeWith(dispose);
 
-                this.WhenAnyFallback(x => x.ViewModel!.CategoryOptions, fallback: default)
-                    .BindTo(this, x => x.GameCategoryOptionsControl.ItemsSource)
+                this.OneWayBind(ViewModel, x => x.CategoryOptions, x => x.GameCategoryOptionsControl.ItemsSource)
                     .DisposeWith(dispose);
                 this.Bind(this.ViewModel, vm => vm.SelectedCategory, view => view.GameCategoryOptionsControl.SelectedItem)
+                    .DisposeWith(dispose);
+
+                this.WhenAnyFallback(x => x.ViewModel!.ReleaseOptions, fallback: default)
+                    .BindTo(this, x => x.GameReleaseOptionsControl.ItemsSource)
+                    .DisposeWith(dispose);
+                this.Bind(this.ViewModel, vm => vm.SelectedRelease, view => view.GameReleaseOptionsControl.SelectedItem)
                     .DisposeWith(dispose);
             });
         }
