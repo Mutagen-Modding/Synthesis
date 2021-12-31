1fa60913ebfa96ee1a743cd2dc6c27fa1a4d374f
Justin Swanson <justin.c.swanson@gmail.com>
2021-12-31 17:39:45 -0600

    File scoped namespace

--- Synthesis.Bethesda.GUI/App.xaml.cs
+++ Synthesis.Bethesda.GUI/App.xaml.cs
@@ -7,48 +7,47 @@ using Synthesis.Bethesda.Execution.Placement;
 using Synthesis.Bethesda.GUI.Services.Startup;
 using Synthesis.Bethesda.GUI.Views;
 
-namespace Synthesis.Bethesda.GUI
+namespace Synthesis.Bethesda.GUI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    protected override void OnStartup(StartupEventArgs e)
     {
-        protected override void OnStartup(StartupEventArgs e)
+        base.OnStartup(e);
+            
+        var singleApp = new SingletonApplicationEnforcer("Synthesis");
+
+        if (!singleApp.IsFirstApplication)
         {
-            base.OnStartup(e);
-            
-            var singleApp = new SingletonApplicationEnforcer("Synthesis");
-
-            if (!singleApp.IsFirstApplication)
-            {
-                Application.Current.Shutdown();
-                return;
-            }
-            
-            var window = new MainWindow();
-            
-            try
-            {
-                var builder = new ContainerBuilder();
-                builder.RegisterModule<Synthesis.Bethesda.GUI.Modules.MainModule>();
-                builder.RegisterInstance(window)
-                    .AsSelf()
-                    .As<IWindowPlacement>()
-                    .As<IMainWindow>();
-                builder.RegisterInstance(singleApp);
-                var container = builder.Build();
-
-                container.Resolve<IStartup>()
-                    .Initialize();
-            }
-            catch (Exception ex)
-            {
-                Log.Logger.Error(ex, "Error constructing container");
-                throw;
-            }
-            
-            window.Show();
+            Application.Current.Shutdown();
+            return;
         }
+            
+        var window = new MainWindow();
+            
+        try
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<Synthesis.Bethesda.GUI.Modules.MainModule>();
+            builder.RegisterInstance(window)
+                .AsSelf()
+                .As<IWindowPlacement>()
+                .As<IMainWindow>();
+            builder.RegisterInstance(singleApp);
+            var container = builder.Build();
+
+            container.Resolve<IStartup>()
+                .Initialize();
+        }
+        catch (Exception ex)
+        {
+            Log.Logger.Error(ex, "Error constructing container");
+            throw;
+        }
+            
+        window.Show();
     }
 }
