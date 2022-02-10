820cdd4f57945993117de9613b154f002b930eeb
Justin Swanson <justin.c.swanson@gmail.com>
2022-02-10 00:07:17 -0600

    Added comment blurb to generated .synth files

--- Synthesis.Bethesda.Execution/FileAssociations/ExportGitAddFile.cs
+++ Synthesis.Bethesda.Execution/FileAssociations/ExportGitAddFile.cs
@@ -1,4 +1,6 @@
-﻿using System.IO.Abstractions;
+﻿using System;
+using System.IO.Abstractions;
+using Microsoft.Build.Logging.StructuredLogger;
 using Newtonsoft.Json;
 using Noggog;
 
@@ -7,6 +9,20 @@ namespace Synthesis.Bethesda.Execution.FileAssociations;
 public class ExportGitAddFile
 {
     private readonly IFileSystem _fileSystem;
+
+    private const string Comment = @"/*
+* This file is to be used with the Synthesis UI.
+*
+* To use this file, first install and download Synthesis:
+* https://github.com/Mutagen-Modding/Synthesis/releases
+*
+* Then double clicking this file will add the desired patcher to the application.  
+* This file alone does not do much and is not necessary.  It is just one way of many to add patchers to Synthesis.
+* 
+* More detailed instructions can be found here:
+* https://github.com/Mutagen-Modding/Synthesis/wiki/Typical-Usage#using-synth-files
+*
+*/";
 
     public ExportGitAddFile(IFileSystem fileSystem)
     {
@@ -28,6 +44,7 @@ public class ExportGitAddFile
         };
         _fileSystem.File.WriteAllText(
             path, 
+            Comment + Environment.NewLine + Environment.NewLine + 
             JsonConvert.SerializeObject(dto, Formatting.Indented, Execution.Constants.JsonSettings));
     }
 }
