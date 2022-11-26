c92ec27a76a7f4fcc7fb37834104ec8a43d7f528
Justin Swanson <justin.c.swanson@gmail.com>
2022-11-26 01:13:17 -0600

    ISettingsDataConfiguration

---
+++ Mutagen.Bethesda.Synthesis/States/ISettingsDataConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace Mutagen.Bethesda.Synthesis;
+
+public interface ISettingsDataConfiguration
+{
+    /// <summary>
+    /// Path to the supplemental data folder dedicated to storing patcher specific user settings/files
+    /// </summary>
+    string ExtraSettingsDataPath { get; }
+
+    /// <summary>
+    /// Path to the internal data folder dedicated to storing patcher specific files hidden from user
+    /// </summary>
+    string? InternalDataPath { get; }
+
+    /// <summary>
+    /// Path to the default data folder as defined by the patcher's source code
+    /// </summary>
+    string? DefaultSettingsDataPath { get; }
+}
