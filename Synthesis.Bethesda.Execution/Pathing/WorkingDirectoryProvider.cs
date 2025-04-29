dc052b7edb08a8ba7b3bc8ce9e216ef1095c9f81
Justin Swanson <justin.c.swanson@gmail.com>
2025-04-29 14:43:20 -0500

    Move working directory to current directory for prod runs
    Some users reporting that temp folder causes permission denied errors

--- Synthesis.Bethesda.Execution/Pathing/WorkingDirectoryProvider.cs
+++ Synthesis.Bethesda.Execution/Pathing/WorkingDirectoryProvider.cs
@@ -10,12 +10,21 @@ public interface IWorkingDirectoryProvider
 
 public class WorkingDirectoryProvider : IWorkingDirectoryProvider
 {
+    private readonly ICurrentDirectoryProvider _currentDirectoryProvider;
     public IEnvironmentTemporaryDirectoryProvider TempDir { get; }
 
-    public WorkingDirectoryProvider(IEnvironmentTemporaryDirectoryProvider tempDir)
+    public WorkingDirectoryProvider(
+        ICurrentDirectoryProvider currentDirectoryProvider, 
+        IEnvironmentTemporaryDirectoryProvider tempDir)
     {
+        _currentDirectoryProvider = currentDirectoryProvider;
         TempDir = tempDir;
+        #if DEBUG
+        WorkingDirectory = Path.Combine(TempDir.Path, "Synthesis")!;
+        #else
+        WorkingDirectory = Path.Combine(_currentDirectoryProvider.CurrentDirectory, "Workspace")!;
+        #endif
     }
         
-    public DirectoryPath WorkingDirectory => Path.Combine(TempDir.Path, "Synthesis")!;
+    public DirectoryPath WorkingDirectory { get; }
 }
